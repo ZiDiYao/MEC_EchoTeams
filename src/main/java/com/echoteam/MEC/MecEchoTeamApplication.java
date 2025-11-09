b55bc6eb6ab757d201e8ccb13a478a5cf45fd8b5
ZiDiYao <zidiyaocanada@outlook.com>
2025-11-09 11:06:53 -0500

    init

---
+++ src/main/java/com/echoteam/MEC/MecEchoTeamApplication.java
@@ -0,0 +1,15 @@
+package com.echoteam.MEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecEchoTeamApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Main start");
+
+        SpringApplication.run(MecEchoTeamApplication.class, args);
+    }
+
+}
